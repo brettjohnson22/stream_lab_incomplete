4f0a2b46cf90bd361f69fe79dac4f9daab16bb4d
brettjohnson22 <br.onson@outlook.com>
2022-07-04 19:14:07 -0500

    upload

---
+++ src/test/java/com/dcc/jpastreamlab/JpaStreamLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.jpastreamlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStreamLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
